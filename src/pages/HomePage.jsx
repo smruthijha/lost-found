56c025a4609aa1978059317ce1ad2bcd5ccd2d23
smruthijha <smruthijha@gmail.com>
2026-04-09 12:34:52 +0530

    fixing homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useItems } from "../context/ItemsContext";
 import ItemCard from "../components/ItemCard";
