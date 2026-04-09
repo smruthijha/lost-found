e6c5804b17db8efa5e8271783a7ef7bd48d01c40
smruthijha <smruthijha@gmail.com>
2026-04-10 02:23:59 +0530

    bug

--- src/pages/PostItemPage.jsx
+++ src/pages/PostItemPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { io } from 'socket.io-client'
-import useAuthStore from '../../store/authStore'
+import { useAuth } from "../../context/AuthContext";
 import WorkerLayout from '../../components/worker/WorkerLayout'
 import { getMyRequestsAPI, updateStatusAPI, getMessagesAPI, sendMessageAPI } from '../../lib/api'
 
